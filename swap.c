f2038f8deba818f442cda276fc5750a5f9a1f6b4
JeevithaL6612 <JEEVITHA.37932@stu.upes.ac.in>
2026-08-18 18:39:32 +0530

    swapping of 2 numbers

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,temp;
+printf("enter first number:");
+scanf("%d",&a);
+printf("enter second number:");
+scanf("%d",&b);
+printf("before swaping: a=%d,b=%d\n",a,b);
+temp=b;
+b=a;
+a=temp;
+printf("after swapping:a=%d,b=%d\n",a,b);
+return 0;
+}
+
