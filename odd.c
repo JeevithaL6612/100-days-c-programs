6207aaa4a5e06167103825f3f0e11b8b0a8d2b65
JeevithaL6612 <JEEVITHA.37932@stu.upes.ac.in>
2026-08-20 11:49:57 +0530

    check if number is even or odd

---
+++ odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+int m;
+printf("enter m:");
+scanf("%d",&m);
+if (m%2==0)
+{
+printf("The given number is even\n");
+}
+else
+{
+printf("The given number is odd\n");
+}
+return 0;
+}
