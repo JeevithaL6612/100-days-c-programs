b08231bce0ff9712eb43b00b4c3602ef66cc3289
JeevithaL6612 <JEEVITHA.37932@stu.upes.ac.in>
2026-08-20 17:09:49 +0530

    to check number is +ve,-ve or zero

---
+++ pnz.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter number:");
+scanf("%d",&num);
+if (num>=0)
+{
+	if(num>=0)
+	{
+	printf("positive\n");
+	}
+	else
+	{
+	printf("zero\n");
+	}
+}
+else
+{
+printf("negative\n");
+}
+return 0;
+}	
