81288380adf0fae47eb651b49fa77476f14be256
JeevithaL6612 <JEEVITHA.37932@stu.upes.ac.in>
2026-08-29 10:40:48 +0530

    basic calculator using switch case

---
+++ cal.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2;
+char op;
+scanf("%d %d %c",&num1,&num2,&op);
+switch(op)
+{
+case'+':
+   printf("%d\n",num1+num2);
+   break;
+case'-':
+   printf("%d\n",num1-num2);
+   break;
+case'*':
+   printf("%d\n",num1*num2);
+   break;
+case'/':
+   if(num2!=0)
+   {
+   printf("%d\n",num1/num2);
+   }
+   else
+   {
+   printf("Error:division by zero\n");
+   }
+   break;
+case'%':
+   if(num2!=0)
+   {
+   printf("%d\n",num1%num2);
+   }
+   else
+   {
+   printf("Error:division by zero\n");
+   }
+   break; 
+default:
+   printf("invalid operator\n");
+}
+return 0;
+}        
+            
