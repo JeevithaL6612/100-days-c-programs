57a8eeea783ec9d775517579e3886a906e70e0c6
JeevithaL6612 <JEEVITHA.37932@stu.upes.ac.in>
2026-09-06 12:54:43 +0530

    sum of series 1

---
+++ series1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n;
+float sum=0.0;
+printf("enter n:");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{
+if(i==1)
+{
+sum*=1.0;
+}
+else
+{
+float numerator=2*i-1;
+float denominator=2*i-2;
+sum+=numerator/denominator;
+}
+}
+printf("approximate sum=%.1f\n",sum);
+return 0;
+}
