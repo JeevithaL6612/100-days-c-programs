6571781dc1ce96428de699795fd4cc6ad09e7d41
JeevithaL6612 <JEEVITHA.37932@stu.upes.ac.in>
2026-09-23 13:15:00 +0530

    even and odd in array

---
+++ eor.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int n,even=0,odd=0;
+printf("enter n:");
+scanf("%d",&n);
+int arr[n];
+for(int i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+if(n%2==0)
+{
+even++;
+}
+else{
+odd++;
+}
+}
+printf("Even=%d,Odd=%d\n",even,odd);
+return 0;
+}
+
